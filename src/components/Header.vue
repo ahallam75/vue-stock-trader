caca3018a1fd2f35794570a6024de493355a292c
ahallam <alexhallam75@gmail.com>
2020-02-15 14:01:43 -0500

    Saving data (PUT request)

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -34,7 +34,7 @@
               >Save & Load <span class="caret"></span
             ></a>
             <ul class="dropdown-menu">
-              <li><a href="#">Save Data</a></li>
+              <li><a href="#" @click="saveData">Save Data</a></li>
               <li><a href="#">Load Data</a></li>
             </ul>
           </li>
@@ -64,6 +64,14 @@ export default {
     ...mapActions(["randomizeStocks"]),
     endDay() {
       this.randomizeStocks();
+    },
+    saveData() {
+      const data = {
+        funds: this.$store.getters.funds,
+        stockPortfolio: this.$store.getters.stockPortfolio,
+        stocks: this.$store.getters.stocks
+      };
+      this.$http.put("data.json", data);
     }
   }
 };
